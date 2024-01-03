e5b3ffe8114a0ff9c656f7c9c0038ea6ce2d00b0
NicolasLoppin66 <loppin.n66@gmail.com>
2024-01-03 21:06:05 +0100

    MAJ : Create Data-Table Component and logic funtion

---
+++ app/payments/page.tsx
@@ -0,0 +1,24 @@
+import { Payment, columns } from "./columns";
+import { DataTable } from "./data-table";
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@exemple.com",
+    },
+  ];
+}
+
+export default async function DemoPage() {
+  const data = await getData();
+
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable Columns={columns} data={data} />
+    </div>
+  );
+}
