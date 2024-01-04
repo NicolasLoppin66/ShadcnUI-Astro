034e2b550601f37af97799048d68a4d9d6ee40bc
NicolasLoppin66 <loppin.n66@gmail.com>
2024-01-04 14:03:56 +0100

    Updating data-table and package.json

--- app/payments/data-table.tsx
+++ app/payments/data-table.tsx
@@ -73,6 +73,10 @@ export function DataTable<TData, TValue>({
 
   return (
     <div>
+      <div className="flex-1 text-sm text-muted-foreground">
+        {table.getFilteredSelectedRowModel().rows.length} of{" "}
+        {table.getFilteredRowModel().rows.length} row(s) selected
+      </div>
       <div className="flex items-center py-4">
         <Input
           placeholder="Filter emails..."
